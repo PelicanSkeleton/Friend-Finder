e68bd813ea1b65ad1141734330755241843d24a1
PelicanSkeleton <sgphillips80@gmail.com>
2020-01-31 16:48:51 -0500

    Needs a few fixes. Close button on modal does not seem to work. Could use some more css.

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+    {
+        name: "Ian McShane",
+        photo: "https://i2.wp.com/www.noisejournal.com/wp-content/uploads/2017/07/Al-Swearengen-Screenshot.jpg?zoom=1.25&resize=678%2C423&ssl=1",
+        scores: [5, 3, 1, 4, 3, 3, 5, 2, 1, 3]
+    },
+    {
+        name: "Timothy Olyphant",
+        photo: "https://static3.srcdn.com/wordpress/wp-content/uploads/2019/07/Timothy-Olyphant-as-Seth-Bullock-in-Deadwood-4.jpg?q=50&fit=crop&w=740&h=370",
+        scores: [1, 4, 3, 1, 3, 3, 1, 5, 4, 3]
+    },
+    {
+        name: "Molly Parker",
+        photo: "https://66.media.tumblr.com/97c8ebe1e169274ba3f5fd2e9d2d28db/tumblr_ne51p1R1jO1u2rmc6o1_500.jpg",
+        scores: [5, 2, 3, 5, 4, 2, 3, 5, 1, 5]
+    },
+    {
+        name: "Charlie Utter",
+        photo: "https://lh3.googleusercontent.com/proxy/stXahJRvJergMA3q8k_35uW2HRfROMJnv_VD88fhC_7DP8VHMjasvEZi5Ffq7tc1gd9CtLJSbrRH7usJ8DdR3iI9ActRGiyTGmzomOEPIYmxwZDdqsMG5gEqqvJyvW-aRVE8RMnLClCL96192siMnGe_J0eEym7ybTTO-xt4VYBWdJg8jdh3adkq2n1KSZCU2g",
+        scores: [3, 4, 1, 1, 5, 2, 2, 1, 3, 4]
+    },
+    {
+        name: "E.B. Farnum",
+        photo: "https://deadwoodchronicles.com/protect/images/DeadwoodS1/Ep07/Ep07-EB.jpg",
+        scores: [2, 5, 5, 4, 1, 2, 4, 2, 1, 1]
+    },
+    {
+        name: "Whitney Ellsworth",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmDJSqMqyabS9pCLROSUpqx1qlO-ft15h05nfKd_ul4X8IPY4tpQ&s",
+        scores: [1, 3, 5, 1, 1, 5, 5, 4, 1, 2]
+    },
+    {
+        name: "Trixie Star",
+        photo: "https://www.hbo.com/content/dam/hbodata/series/deadwood/character/trixie-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+        scores: [3, 3, 3, 4, 5, 2, 5, 1, 1, 4]
+    },
+]
+
+module.exports = friends;
